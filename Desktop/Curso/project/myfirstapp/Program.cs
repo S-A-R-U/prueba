515e41729f2d040200e6882ea0756b243108f565
S-A-R-U <matiaseas@gmail.com>
2022-03-09 16:56:19 -0300

    git

---
+++ Desktop/Curso/project/myfirstapp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+namespace myfirstapp
+{
+class program
+    {
+        static void Main(string []args)
+          {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("este es mi primer programa en CS");
+
+          }  
+
+
+
+    }
+
+
+
+ }
